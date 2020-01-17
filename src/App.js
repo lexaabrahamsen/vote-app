f2c737395591aa5c4e00bec40202fe9feb31fae8
Lexa <lexaraea@gmail.com>
2020-01-17 09:12:46 -0500

    Updating state of vote count

--- src/App.js
+++ src/App.js
@@ -4,40 +4,72 @@ import "./App.css";
 const list = [
   {
     id: 1,
-    name: "John Lennon"
+    name: "John Lennon",
+    votes: 0
   },
   {
     id: 2,
-    name: "Paul McCartney"
+    name: "Paul McCartney",
+    votes: 0
   },
   {
     id: 3,
-    name: "George Harrison"
+    name: "George Harrison",
+    votes: 0
   },
   {
     id: 4,
-    name: "Ringo Starr"
+    name: "Ringo Starr",
+    votes: 0
   }
 ];
 
 class App extends Component {
-  handleEvent = e => console.log("button clicked for " + e);
+  state = {
+    members: []
+  };
+
+  componentDidMount() {
+    this.setState({ members: list });
+  }
+
+  handleEvent = memberId => {
+    const updatedList = this.state.members.map(member => {
+      if (member.id === memberId) {
+        return Object.assign({}, member, {
+          votes: member.votes + 1
+        });
+      } else {
+        return member;
+      }
+    });
+
+    this.setState({
+      members: updatedList
+    });
+  };
 
   render() {
-    return list.map(member => (
-      <Beatle key={member.id} name={member.name} onVote={this.handleEvent} />
+    return this.state.members.map(member => (
+      <Beatle
+        key={member.id}
+        id={member.id}
+        name={member.name}
+        votes={member.votes}
+        onVote={this.handleEvent}
+      />
     ));
   }
 }
 
 class Beatle extends Component {
-  handleClick = () => this.props.onVote(this.props.name);
+  handleClick = () => this.props.onVote(this.props.id);
 
   render() {
     return (
       <div className="App">
         {this.props.name}
-        <button onClick={this.handleClick}>+</button>
+        <button onClick={this.handleClick}>+</button> {this.props.votes}
       </div>
     );
   }
