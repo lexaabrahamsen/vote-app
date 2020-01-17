8f7938a56b0f71abae7cb28ff92026461cf7a630
Lexa <lexaraea@gmail.com>
2020-01-16 20:47:19 -0500

    Add member

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Beatle name="John Lennon" />;
+  }
+}
+
+class Beatle extends Component {
+  render() {
+    return <div className="App">{this.props.name}</div>;
+  }
 }
 
 export default App;
