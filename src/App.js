702e8df528ae0eb687db8e762ee8c4efa72654b5
Lexa <lexaraea@gmail.com>
2020-01-16 20:51:42 -0500

    Add loop for members

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const list = [
+  {
+    name: "John Lennon"
+  },
+  {
+    name: "Paul McCartney"
+  },
+  {
+    name: "George Harrison"
+  },
+  {
+    name: "Ringo Starr"
+  }
+];
+
 class App extends Component {
   render() {
-    return <Beatle name="John Lennon" />;
+    return list.map(member => <Beatle name={member.name} />);
   }
 }
 
